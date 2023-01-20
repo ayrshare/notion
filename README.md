ed506b6c4a45dc0f721c6551fac27cfceb79b6bc
Geoffrey Bourne <gbourne@gmail.com>
2023-01-20 15:38:33 -0500

    updated Readme and global variables

--- README.md
+++ README.md
@@ -1,46 +1,48 @@
 # Connect Notion to Ayshare API
 
-Using this script, you can use post data from a Notion database to make posts to your social networks. 
+Post to your social media networks directly from Notion. See the Notion Integration docs for details.
+
+Supports Node 18+
 
 ## Create a Notion database
+
 In Notion, create a database in table view with the following column names and column types:
 
 * **Post** as Title column type (you don’t have a choice here with the column type)
 * **Platforms** as Multi Select column type with values: facebook, instagram, twitter, linkedin, reddit, and telegram
-* **Images** as Files & Media column type 
+* **Images** as Files & Media column type
 * **Profile Keys** as Text column type
 * **Status** as Text column type
 * **Schedule Date** as Date column type with Date Format Month/Day/Year, Time Format 24 Hours, and include time
 
 These fields will be used in the script specified later in this page. 
 
-See a live Notion example: https://ayrshare-example.notion.site/607c15ce7872456a879adbb0a5f17fdf?v=ee9f65a4afb24033813f245a45bc9e83 
- 
+[See a live Notion example](https://ayrshare-example.notion.site/607c15ce7872456a879adbb0a5f17fdf?v=ee9f65a4afb24033813f245a45bc9e83)
 
 ### Enter in Test Post Data
 
 We need some sample data to test the post. Here is a suggestion:
 
-* **Post**: Enter - Happy New Year 2023
+* **Post**: Enter - Happy New Year!
 * **Platforms**: select one or more networks you have linked. Please be sure the name is lowercase.
-* **Images**: Attach an image. We like this one you can download and attach: https://img.ayrshare.com/012/gb.jpg
+* **Images**: Attach an [image](https://img.ayrshare.com/012/gb.jpg). We like this one you can download and attach.
 * **Profile Keys**: If you are on the Business Plan and want to post to a client's profile, enter their * Profile Key. Otherwise, leave blank.
 * **Status**: Enter - pending. The script only grabs records that are set to pending. Please be sure "pending” is lowercase.
 * **Schedule Date**: Leave blank since we'll just test immediate posting right now. Later you can select a future date to schedule the post.
 
 ## Create Internal Integration in Notion
 
-Go to the My Integrations page in Notion: https://www.notion.so/my-integrations and click on New Integration. 
+Go to the [My Integrations page](https://www.notion.so/my-integrations) in Notion and click on New Integration.
 
 You can name the integration ‘Ayrshare’ to identify it easily and choose the appropriate workspace that will have the post data.
- 
-Finally, the default capabilities that have been selected for you will do. Submit to create the integration. 
 
-If successful, an internal integration token will be available to you. Note this for future steps in this page. 
+Finally, the default capabilities that have been selected for you will do. Submit to create the integration.
+
+If successful, an internal integration token will be available to you. Note this for future steps in this page.
 
 ## Connect the Notion database to the Internal Integration
 
-Open the Notion database that you created earlier. Click on the ellipsis on the top right corner of the page and go to ‘Add Connection’. 
+Open the Notion database that you created earlier. Click on the ellipsis on the top right corner of the page and go to ‘Add Connection’.
 
 Here you can search for the internal integration you created in the previous step by the name that you chose for it. 
 
@@ -52,8 +54,8 @@ You can now run a script in your local environment that will read data from the 
 
 Make sure to set the following environment variables used in the script:
 
-* API_KEY: this is API Key you get from Ayrshare
-* NOTION_DATABASE_ID: Open the database you created earlier in this page and get the database ID from the URL. It will be in this format: https://www.notion.so/<NOTION_DATABASE_ID>?v=ee9f65a4afb24033813f245a45bc9e83
-* NOTION_KEY: internal integration token from earlier
+* `API_KEY`: this is API Key you get from Ayrshare
+* `NOTION_DATABASE_ID`: Open the database you created earlier in this page and get the database ID from the URL. It will be in this format: `https://www.notion.so/<NOTION_DATABASE_ID>?v=ee9f65a4afb24033813f245a45bc9e83`
+* `NOTION_KEY`: internal integration token from earlier
 
 Run the script. You can run it at [Heroku](https://www.heroku.com/), [Digital Ocean](https://www.digitalocean.com/), or [Vercel](https://vercel.com/) for example. If successful, all ‘pending’ status columns will be changed to ‘success’ and the posts will have been made to the appropriate social networks.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-const moment = require('moment');
+const dotenv = require("dotenv");
+const moment = require("moment");
 
 dotenv.config();
 
@@ -7,135 +7,161 @@ const API_KEY = process.env.API_KEY;
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 const NOTION_KEY = process.env.NOTION_KEY;
 
+const NOTION_BASE_PAGES = "https://api.notion.com/v1/pages/";
+const NOTION_BASE_DB = "https://api.notion.com/v1/databases/";
+const AYRSHARE_BASE_URL = "https://app.ayrshare.com/api/";
+
 const sendPost = async (data) => {
-    const { post, platforms, imageUrls, profileKeys, scheduleDate, shortenLinks } = data;
+  const {
+    post,
+    platforms,
+    imageUrls,
+    profileKeys,
+    scheduleDate,
+    shortenLinks,
+  } = data;
 
-    const body = Object.assign({},
-        post && { post },
-        platforms && { platforms },
-        profileKeys && { profileKeys: profileKeys.split(',').map(profileKey => profileKey.trim()) },
-        Array.isArray(imageUrls) && imageUrls.length > 0 && { mediaUrls: imageUrls },
-        scheduleDate && { scheduleDate },
-        shortenLinks !== undefined && shortenLinks !== null && { shortenLinks }
-    );
+  const body = Object.assign(
+    {},
+    post && { post },
+    platforms && { platforms },
+    profileKeys && {
+      profileKeys: profileKeys
+        .split(",")
+        .map((profileKey) => profileKey.trim()),
+    },
+    Array.isArray(imageUrls) &&
+      imageUrls.length > 0 && { mediaUrls: imageUrls },
+    scheduleDate && { scheduleDate },
+    shortenLinks !== undefined && shortenLinks !== null && { shortenLinks }
+  );
 
-    console.log('Posting JSON:', JSON.stringify(body, null, 2));
+  console.log("Posting JSON:", JSON.stringify(body, null, 2));
 
-    if (profileKeys) {
-        body.profileKeys = profileKeys.split(",");
-    }
+  if (profileKeys) {
+    body.profileKeys = profileKeys.split(",");
+  }
 
-    const response = await fetch('https://app.ayrshare.com/api/post', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_KEY}`
-        }
-    }).then(res => res.json());
+  const response = await fetch(`${AYRSHARE_BASE_URL}post`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  }).then((res) => res.json());
 
-    return response;
+  return response;
 };
 
 const getPostsFromNotion = async () => {
-    const pages = [];
-    let cursor = undefined;
-    const body = {
-        filter: {
-            property: 'Status',
-            rich_text: {
-                equals: 'pending'
-            }
-        }
-    };
+  const pages = [];
+  let cursor = undefined;
+  const body = {
+    filter: {
+      property: "Status",
+      rich_text: {
+        equals: "pending",
+      },
+    },
+  };
 
-    while (true) {
-        const { results, next_cursor } = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${NOTION_KEY}`,
-                'Notion-Version': '2022-06-28'
-            }
-        }).then(res => res.json());
-        pages.push(...results)
-        if (!next_cursor) {
-            break
-        }
-        cursor = next_cursor
+  while (true) {
+    const { results, next_cursor } = await fetch(
+      `${NOTION_BASE_DB}${DATABASE_ID}/query`,
+      {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${NOTION_KEY}`,
+          "Notion-Version": "2022-06-28",
+        },
+      }
+    ).then((res) => res.json());
+    pages.push(...results);
+    if (!next_cursor) {
+      break;
     }
-    console.log(`${pages.length} post(s) successfully fetched.`);
+    cursor = next_cursor;
+  }
+  
+  console.log(`${pages.length} post(s) successfully fetched.`);
 
-    const posts = []
-    for (const page of pages) {
-        const postId = page.properties['Post'].id;
-        const platformsId = page.properties['Platforms'].id;
-        const imagesId = page.properties['Images'].id;
-        const profileKeysId = page.properties['Profile Keys'].id;
-        const scheduleDateId = page.properties['Schedule Date'].id
-        const { properties } = await fetch(`https://api.notion.com/v1/pages/${page.id}?filter_properties=${postId}&filter_properties=${platformsId}&filter_properties=${imagesId}&filter_properties=${profileKeysId}&filter_properties=${scheduleDateId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${NOTION_KEY}`,
-                'Notion-Version': '2022-06-28'
-            }
-        }).then(res => res.json());
-        const post = properties.Post.title[0];
-        const profileKeys = properties['Profile Keys'].rich_text[0];
-        const scheduleDate = properties['Schedule Date'].date
-        posts.push({
-            pageId: page.id,
-            ...(post && { post: post.plain_text }),
-            platforms: properties.Platforms.multi_select.map(multiSelect => multiSelect.name),
-            imageUrls: properties.Images.files.map(file => file.file.url),
-            ...(profileKeys && { profileKeys: profileKeys.plain_text }),
-            ...(scheduleDate && { scheduleDate: moment(JSON.stringify(scheduleDate.start), 'YYY-MM-DDTHH:mm:ssZ').toISOString() })
-        });
-    }
+  const posts = [];
+  for (const page of pages) {
+    const postId = page.properties["Post"].id;
+    const platformsId = page.properties["Platforms"].id;
+    const imagesId = page.properties["Images"].id;
+    const profileKeysId = page.properties["Profile Keys"].id;
+    const scheduleDateId = page.properties["Schedule Date"].id;
+    const { properties } = await fetch(
+      `${NOTION_BASE_PAGES}${page.id}?filter_properties=${postId}&filter_properties=${platformsId}&filter_properties=${imagesId}&filter_properties=${profileKeysId}&filter_properties=${scheduleDateId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${NOTION_KEY}`,
+          "Notion-Version": "2022-06-28",
+        },
+      }
+    ).then((res) => res.json());
+    const post = properties.Post.title[0];
+    const profileKeys = properties["Profile Keys"].rich_text[0];
+    const scheduleDate = properties["Schedule Date"].date;
+    posts.push({
+      pageId: page.id,
+      ...(post && { post: post.plain_text }),
+      platforms: properties.Platforms.multi_select.map(
+        (multiSelect) => multiSelect.name
+      ),
+      imageUrls: properties.Images.files.map((file) => file.file.url),
+      ...(profileKeys && { profileKeys: profileKeys.plain_text }),
+      ...(scheduleDate && {
+        scheduleDate: moment(
+          JSON.stringify(scheduleDate.start),
+          "YYY-MM-DDTHH:mm:ssZ"
+        ).toISOString(),
+      }),
+    });
+  }
 
-    return posts;
-}
-
+  return posts;
+};
 
 const makePostsFromNotion = async () => {
-    const posts = await getPostsFromNotion();
-    for (let post of posts) {
+  const posts = await getPostsFromNotion();
+  for (let post of posts) {
+    const shortenLinks = false;
+    const response = await sendPost({ ...post, shortenLinks });
 
-        const shortenLinks = false;
-        const response = await sendPost({ ...post, shortenLinks });
+    if (response) {
+      let status;
+      if (Array.isArray(response)) {
+        status = response.map((x) => x.status).every((x) => x === "success")
+          ? "success"
+          : "error";
+      } else {
+        status = response.status;
+      }
+      if (status === "error") {
+        console.error(JSON.stringify(response));
+      }
 
-        if (response) {
-            let status;
-            if (Array.isArray(response)) {
-                status = response.map(x => x.status).every(x => x === "success") ? "success" : "error";
-            } else {
-                status = response.status;
-            }
-            if (status === 'error') {
-                console.error(JSON.stringify(response));
-            }
-
-            const body = { 
-                properties: 
-                    { Status: 
-                        { rich_text: 
-                            [{ text: { content: status } }] 
-                        } 
-                    } 
-                };
-            await fetch(`https://api.notion.com/v1/pages/${post.pageId}`, {
-                method: 'PATCH',
-                body: JSON.stringify(body),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${NOTION_KEY}`,
-                    'Notion-Version': '2022-06-28'
-                }
-            });
-        }
+      const body = {
+        properties: { Status: { rich_text: [{ text: { content: status } }] } },
+      };
+      await fetch(`${NOTION_BASE_PAGES}${post.pageId}`, {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${NOTION_KEY}`,
+          "Notion-Version": "2022-06-28",
+        },
+      });
     }
-}
+  }
+};
 
 makePostsFromNotion();
